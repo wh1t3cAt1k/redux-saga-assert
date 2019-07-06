0642988fa4ed5ff240415a51522290f42ed91fb3
wh1t3cAt1k <pavel@kabir.ru>
2019-07-06 13:44:38 +0300

    Support multiple actions

--- src/AssertSaga.test.ts
+++ src/AssertSaga.test.ts
@@ -1,9 +1,9 @@
 import { SagaType } from 'redux-saga-test-plan';
 import * as effects from 'redux-saga/effects';
 import { createAction, getType } from 'typesafe-actions';
-import { assertSaga, SagaAssertImplementation } from './AssertSaga';
+import { assertSaga } from './AssertSaga';
 
-// tslint:disable:no-console
+// tslint:disable:no-console mocha-no-side-effect-code
 
 const consoleError = console.error;
 
@@ -11,175 +11,152 @@ beforeEach(() => {
     console.error = consoleError;
 });
 
-// tslint:disable-next-line:mocha-no-side-effect-code
-const action = createAction('some/ACTION');
-
-function* watcherRespondingToLatest() {
-    yield effects.takeLatest(
-        getType(action),
-        handlerSagaStub
-    );
-}
-
-function* watcherRespondingToLatestWithExtraWork() {
-    yield effects.delay(42);
-    yield effects.takeLatest(
-        getType(action),
-        handlerSagaStub
-    );
-}
-
-function* watcherRespondingToEvery() {
-    yield effects.takeEvery(
-        getType(action),
-        handlerSagaStub
-    );
-}
-
-function* watcherRespondingToEveryWithExtraWork() {
-    yield effects.delay(42);
-    yield effects.takeEvery(
-        getType(action),
-        handlerSagaStub
-    );
-}
-
-function* watcherRespondingToLeading() {
-    yield effects.takeLeading(
-        getType(action),
-        handlerSagaStub
-    );
-}
-
-function* watcherRespondingToLeadingWithExtraWork() {
-    yield effects.delay(42);
-    yield effects.takeLeading(
-        getType(action),
-        handlerSagaStub
-    );
-}
+const relevantAction = createAction('some/ACTION');
+const anotherRelevantAction = createAction('some/OTHER_ACTION');
+const irrelevantAction = createAction('some/IRRELEVANT_ACTION');
 
 function* handlerSagaStub() {
     yield undefined;
 }
 
-describe(SagaAssertImplementation.prototype.justRespondsToLatest, () => {
-    it('passes when saga responds to latest specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLatest)
-            .justRespondsToLatest({ action, withHandler: handlerSagaStub });
+function* irrelevantHandlerSagaStub() {
+    yield 42;
+}
 
-        expect(assertionOperation)
-            .not
-            .toThrow();
+function* watcherRespondingToLatest() {
+    yield effects.takeLatest(
+        [getType(relevantAction), getType(anotherRelevantAction)],
+        handlerSagaStub
+    );
+}
+
+function* watcherRespondingToIrrelevantLatest() {
+    yield effects.takeLatest(getType(irrelevantAction), handlerSagaStub);
+}
+
+function* watcherRespondingToLatestWithIncorrectHandler() {
+    yield effects.takeLatest(
+        getType(relevantAction),
+        irrelevantHandlerSagaStub
+    );
+}
+
+function* watcherRespondingToInsufficientLatest() {
+    yield effects.takeLatest([getType(relevantAction)], handlerSagaStub);
+}
+
+function* watcherRespondingToLatestWithExtraWork() {
+    yield effects.delay(42);
+    yield effects.takeLatest(getType(relevantAction), handlerSagaStub);
+}
+
+function* correctWatcherRespondingToEvery() {
+    yield effects.takeEvery(getType(relevantAction), handlerSagaStub);
+}
+
+function* watcherRespondingToLeading() {
+    yield effects.takeLeading(getType(relevantAction), handlerSagaStub);
+}
+
+const getAssertionOperation = (saga: SagaType) => () =>
+    assertSaga(saga).justRespondsToLatest({
+        actions: [anotherRelevantAction, relevantAction],
+        withHandler: handlerSagaStub,
+    });
+
+describe(assertSaga(handlerSagaStub).justRespondsToLatest, () => {
+    it('passes when saga responds to latest specified actions regardless of order', () => {
+        expect(
+            getAssertionOperation(watcherRespondingToLatest)
+        ).not.toThrow();
+    });
+
+    it('passes regardless of whether action argument is an array or a single action', () => {
+        function* watcherRespondingToOneLatestWithNonArraySignature() {
+            yield effects.takeLatest(getType(relevantAction), handlerSagaStub);
+        }
+
+        expect(() =>
+            assertSaga(
+                watcherRespondingToOneLatestWithNonArraySignature
+            ).justRespondsToLatest({
+                action: relevantAction,
+                withHandler: handlerSagaStub,
+            })
+        ).not.toThrow();
     });
 
     it('throws when saga does not respond to latest specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToEvery)
-            .justRespondsToLatest({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .toThrowError();
+        expect(
+            getAssertionOperation(correctWatcherRespondingToEvery)
+        ).toThrowError();
     });
 
     it('throws when saga responds to latest and also contains something else', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLatestWithExtraWork)
-            .justRespondsToLatest({ action, withHandler: handlerSagaStub });
+        expect(
+            getAssertionOperation(watcherRespondingToLatestWithExtraWork)
+        ).toThrowError();
+    });
 
-        expect(assertionOperation)
-            .toThrowError();
+    it('throws when saga responds to incorrect latest action', () => {
+        expect(
+            getAssertionOperation(watcherRespondingToIrrelevantLatest)
+        ).toThrowError();
+    });
+
+    it('throws when saga responds to latest action with incorrect handler', () => {
+        expect(
+            getAssertionOperation(watcherRespondingToLatestWithIncorrectHandler)
+        ).toThrowError();
+    });
+
+    it('throws when saga responds to insufficient actions', () => {
+        expect(
+            getAssertionOperation(watcherRespondingToInsufficientLatest)
+        ).toThrowError();
     });
 });
 
-describe(SagaAssertImplementation.prototype.justRespondsToEvery, () => {
-    it('passes when saga responds to every specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToEvery)
-            .justRespondsToEvery({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .not
-            .toThrow();
-    });
-
-    it('throws when saga does not respond to every specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLatest)
-            .justRespondsToEvery({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .toThrowError();
-    });
-
-    it('throws when saga responds to every and also contains something else', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToEveryWithExtraWork)
-            .justRespondsToEvery({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .toThrowError();
-    });
-});
-
-describe(SagaAssertImplementation.prototype.justRespondsToLeading, () => {
-    it('passes when saga responds to leading specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLeading)
-            .justRespondsToLeading({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .not
-            .toThrow();
-    });
-
-    it('throws when saga does not respond to leading specified action', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLatest)
-            .justRespondsToLeading({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .toThrowError();
-    });
-
-    it('throws when saga responds to leading and also contains something else', () => {
-        const assertionOperation = () => assertSaga(watcherRespondingToLeadingWithExtraWork)
-            .justRespondsToLeading({ action, withHandler: handlerSagaStub });
-
-        expect(assertionOperation)
-            .toThrowError();
-    });
-});
-
-describe(SagaAssertImplementation.prototype.justSpawnsAsync, () => {
+describe(assertSaga(handlerSagaStub).justSpawnsAsync, () => {
     it.each<[SagaType]>([
-        [function* () {
-            yield effects.all([
-                effects.spawn(watcherRespondingToEvery),
-                effects.spawn(watcherRespondingToLatest),
-                effects.spawn(watcherRespondingToLeading),
-            ]);
-        }],
-        [function* () {
-            yield effects.spawn(watcherRespondingToEvery);
-            yield effects.spawn(watcherRespondingToLatest);
-            yield effects.spawn(watcherRespondingToLeading);
-        }],
+        [
+            function*() {
+                yield effects.all([
+                    effects.spawn(correctWatcherRespondingToEvery),
+                    effects.spawn(watcherRespondingToLatest),
+                    effects.spawn(watcherRespondingToLeading),
+                ]);
+            },
+        ],
+        [
+            function*() {
+                yield effects.spawn(correctWatcherRespondingToEvery);
+                yield effects.spawn(watcherRespondingToLatest);
+                yield effects.spawn(watcherRespondingToLeading);
+            },
+        ],
     ])(
         'passes when saga spawns the specified sagas regardless of order and does nothing else',
         async saga => {
-            return assertSaga(saga)
-                .justSpawnsAsync(
-                    watcherRespondingToLeading,
-                    watcherRespondingToLatest,
-                    watcherRespondingToEvery
-                );
+            return assertSaga(saga).justSpawnsAsync(
+                watcherRespondingToLeading,
+                watcherRespondingToLatest,
+                correctWatcherRespondingToEvery
+            );
         }
     );
 
     it('fails when saga forks instead of spawns', async () => {
         console.error = jest.fn();
 
-        const saga = function* () {
+        const saga = function*() {
             yield effects.fork(watcherRespondingToLatest);
         };
 
         try {
-            await assertSaga(saga)
-                .justSpawnsAsync(watcherRespondingToLatest);
+            await assertSaga(saga).justSpawnsAsync(
+                watcherRespondingToLatest
+            );
         } catch {
             return;
         }
@@ -190,14 +167,17 @@ describe(SagaAssertImplementation.prototype.justSpawnsAsync, () => {
     it('fails when saga contains additional call effects', async () => {
         console.error = jest.fn();
 
-        const saga = function* () {
-            yield effects.spawn(watcherRespondingToEvery);
-            yield effects.call(() => { /* do nothing */ });
+        const saga = function*() {
+            yield effects.spawn(correctWatcherRespondingToEvery);
+            yield effects.call(() => {
+                /* do nothing */
+            });
         };
 
         try {
-            await assertSaga(saga)
-                .justSpawnsAsync(watcherRespondingToEvery);
+            await assertSaga(saga).justSpawnsAsync(
+                correctWatcherRespondingToEvery
+            );
         } catch {
             return;
         }
@@ -208,16 +188,17 @@ describe(SagaAssertImplementation.prototype.justSpawnsAsync, () => {
     it('fails when saga contains additional put effects', async () => {
         console.error = jest.fn();
 
-        const saga = function* () {
-            yield effects.spawn(watcherRespondingToEvery);
+        const saga = function*() {
+            yield effects.spawn(correctWatcherRespondingToEvery);
             yield effects.put({
                 type: 'action/ON_ANY_ACTION',
             });
         };
 
         try {
-            await assertSaga(saga)
-                .justSpawnsAsync(watcherRespondingToEvery);
+            await assertSaga(saga).justSpawnsAsync(
+                correctWatcherRespondingToEvery
+            );
         } catch {
             return;
         }
@@ -228,14 +209,15 @@ describe(SagaAssertImplementation.prototype.justSpawnsAsync, () => {
     it('fails when saga contains additional spawn effects', async () => {
         console.error = jest.fn();
 
-        const saga = function* () {
-            yield effects.spawn(watcherRespondingToEvery);
+        const saga = function*() {
+            yield effects.spawn(correctWatcherRespondingToEvery);
             yield effects.spawn(watcherRespondingToLatest);
         };
 
         try {
-            await assertSaga(saga)
-                .justSpawnsAsync(watcherRespondingToEvery);
+            await assertSaga(saga).justSpawnsAsync(
+                correctWatcherRespondingToEvery
+            );
         } catch {
             return;
         }
@@ -246,16 +228,15 @@ describe(SagaAssertImplementation.prototype.justSpawnsAsync, () => {
     it('fails when saga contains insufficient spawn effects', async () => {
         console.error = jest.fn();
 
-        const saga = function* () {
-            yield effects.spawn(watcherRespondingToEvery);
+        const saga = function*() {
+            yield effects.spawn(correctWatcherRespondingToEvery);
         };
 
         try {
-            await assertSaga(saga)
-                .justSpawnsAsync(
-                    watcherRespondingToEvery,
-                    watcherRespondingToLatest
-                );
+            await assertSaga(saga).justSpawnsAsync(
+                correctWatcherRespondingToEvery,
+                watcherRespondingToLatest
+            );
         } catch {
             return;
         }
--- src/AssertSaga.ts
+++ src/AssertSaga.ts
@@ -1,12 +1,27 @@
 import _ from 'lodash';
 import { ActionCreator, AnyAction } from 'redux';
-import { expectSaga, SagaType, testSaga } from 'redux-saga-test-plan';
+import { expectSaga, SagaType } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
 import * as providers from 'redux-saga-test-plan/providers';
 import * as effects from 'redux-saga/effects';
 import { getType } from 'typesafe-actions';
 
-export class SagaAssertImplementation {
+type SingleActionAssertionArguments = {
+    action: ActionCreator<AnyAction>;
+    withHandler: SagaType;
+};
+
+type MultipleActionsAssertionArguments = {
+    actions: ReadonlyArray<ActionCreator<AnyAction>>;
+    withHandler: SagaType;
+};
+
+const isSingleActionAssertion = (
+    args: SingleActionAssertionArguments | MultipleActionsAssertionArguments
+): args is SingleActionAssertionArguments =>
+    (args as SingleActionAssertionArguments).action !== undefined;
+
+class SagaAssertImplementation {
     private readonly saga: SagaType;
 
     public constructor(saga: SagaType) {
@@ -18,55 +33,26 @@ export class SagaAssertImplementation {
      * with the specified handler saga and does absolutely nothing else.
      * Requires enhanced action creator from typesafe-actions.
      */
-    public justRespondsToLeading = (args: {
-        action: ActionCreator<AnyAction>;
-        withHandler: SagaType;
-    }) => {
-        const saga = this.saga();
-
-        expect(saga.next().value)
-            .toEqual(effects.takeLeading(
-                getType(args.action),
-                args.withHandler
-            ));
-
-        expect(saga.next().done)
-            .toBe(true);
-    }
-
+    public justRespondsToLeading = (
+        args: SingleActionAssertionArguments | MultipleActionsAssertionArguments
+    ) => this.justRespondsTo(args, effects.takeLeading);
     /**
      * Ensures that the tested saga only responds to the latest specified action
      * with the specified handler saga and does absolutely nothing else.
      * Requires enhanced action creator from typesafe-actions.
      */
-    public justRespondsToLatest = (args: {
-        action: ActionCreator<AnyAction>;
-        withHandler: SagaType;
-    }) => testSaga(this.saga)
-        .next()
-        .takeLatest(
-            getType(args.action),
-            args.withHandler
-        )
-        .next()
-        .isDone()
+    public justRespondsToLatest = (
+        args: SingleActionAssertionArguments | MultipleActionsAssertionArguments
+    ) => this.justRespondsTo(args, effects.takeLatest);
 
     /**
      * Ensures that the tested saga only responds to every specified action
      * with the specified handler saga and does absolutely nothing else.
      * Requires enhanced action creator from typesafe-actions.
      */
-    public justRespondsToEvery = (args: {
-        action: ActionCreator<AnyAction>;
-        withHandler: SagaType;
-    }) => testSaga(this.saga)
-        .next()
-        .takeEvery(
-            getType(args.action),
-            args.withHandler
-        )
-        .next()
-        .isDone()
+    public justRespondsToEvery = (
+        args: SingleActionAssertionArguments | MultipleActionsAssertionArguments
+    ) => this.justRespondsTo(args, effects.takeEvery);
 
     /**
      * Ensures that the tested saga only spawns (not forks) the specified
@@ -79,39 +65,97 @@ export class SagaAssertImplementation {
                 [
                     matchers.spawn.like({}),
                     providers.dynamic<effects.ForkEffectDescriptor>(effect => {
-                        throw new Error(`Unexpected function spawned: ${effect.fn.name}`);
+                        throw new Error(
+                            `Unexpected function spawned: ${effect.fn.name}`
+                        );
                     }),
                 ],
                 [
                     matchers.fork.like({}),
                     providers.dynamic<effects.ForkEffectDescriptor>(effect => {
-                        throw new Error(`Unexpected function forked: ${effect.fn.name}`);
+                        throw new Error(
+                            `Unexpected function forked: ${effect.fn.name}`
+                        );
                     }),
                 ],
                 [
                     matchers.put.like({}),
-                    providers.dynamic<effects.PutEffectDescriptor<AnyAction>>(effect => {
-                        throw new Error(`Unexpected action put: ${effect.action.type}`);
-                    }),
+                    providers.dynamic<effects.PutEffectDescriptor<AnyAction>>(
+                        effect => {
+                            throw new Error(
+                                `Unexpected action put: ${effect.action.type}`
+                            );
+                        }
+                    ),
                 ],
                 [
                     matchers.call.like({}),
                     providers.dynamic<effects.CallEffectDescriptor>(effect => {
-                        throw new Error(`Unexpected function called: ${effect.fn.name}`);
+                        throw new Error(
+                            `Unexpected function called: ${effect.fn.name}`
+                        );
                     }),
                 ],
             ]);
 
         let expectApiBuilder = expectSaga(this.saga)
             // tslint:disable-next-line: no-any
-            .provide(<any> providedValues);
+            .provide(providedValues as any);
 
         sagasToSpawn.forEach(sagaType => {
             expectApiBuilder = expectApiBuilder.spawn(sagaType);
         });
 
         return expectApiBuilder.run();
-    }
+    };
+
+    private readonly justRespondsTo = (
+        args:
+            | SingleActionAssertionArguments
+            | MultipleActionsAssertionArguments,
+        createEffect: (pattern: string[], handler: SagaType) => unknown
+    ) => {
+        const actions = isSingleActionAssertion(args)
+            ? [args.action]
+            : args.actions;
+        const actionTypes = actions
+            .map(action => getType(action) as string)
+            .sort();
+
+        const saga = this.saga();
+        const nextSagaValue = saga.next().value;
+
+        if (
+            nextSagaValue === undefined ||
+            nextSagaValue.payload === undefined ||
+            nextSagaValue.payload.args === undefined
+        ) {
+            throw Error(
+                'Expected the first generator value to be a redux-saga effect.'
+            );
+        }
+
+        const actionTypesArgument = nextSagaValue.payload.args[0];
+
+        if (typeof actionTypesArgument === 'string') {
+            // This is a single action type value. Normalize.
+            // -
+            nextSagaValue.payload.args[0] = [actionTypesArgument];
+        }
+
+        if (Array.isArray(actionTypesArgument)) {
+            // To be order-agnostic, we sort the actual action types.
+            // -
+            actionTypesArgument.sort();
+        }
+
+        expect(nextSagaValue).toEqual(
+            createEffect(actionTypes, args.withHandler)
+        );
+
+        expect(saga.next().done).toBe(true);
+    };
 }
 
-export const assertSaga = (saga: SagaType) => new SagaAssertImplementation(saga);
+export const assertSaga = (saga: SagaType) =>
+    new SagaAssertImplementation(saga);
