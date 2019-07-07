43699eb5d65987caf1acfe98323b99e6a3224248
wh1t3cAt1k <pavel@kabir.ru>
2019-07-07 13:40:23 +0300

    Remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// tslint:disable:file-name-casing
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./AssertSaga"));
